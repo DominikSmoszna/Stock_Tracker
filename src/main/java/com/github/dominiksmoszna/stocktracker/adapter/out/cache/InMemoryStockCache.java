1fa4e9c40e89daed5fa8a8b16913bf403aaf107c
Dominik Smoszna <dominiksmoszna@gmail.com>
2026-04-07 13:43:20 +0200

    feat: implement InMemoryStockCache

---
+++ src/main/java/com/github/dominiksmoszna/stocktracker/adapter/out/cache/InMemoryStockCache.java
@@ -0,0 +1,24 @@
+package com.github.dominiksmoszna.stocktracker.adapter.out.cache;
+
+import com.github.dominiksmoszna.stocktracker.domain.model.StockPrice;
+import com.github.dominiksmoszna.stocktracker.domain.port.out.CurrentPricePort;
+import com.github.dominiksmoszna.stocktracker.domain.port.out.SaveCurrentPricePort;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class InMemoryStockCache implements CurrentPricePort, SaveCurrentPricePort {
+
+    ConcurrentHashMap<String, StockPrice> stockPrices = new ConcurrentHashMap<>();
+
+    @Override
+    public StockPrice getCurrentPrice(String ticker) {
+        return stockPrices.get(ticker);
+    }
+
+    @Override
+    public void saveCurrentPrice(StockPrice stockPrice) {
+        stockPrices.put(stockPrice.getTicker(), stockPrice);
+    }
+}
