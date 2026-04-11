333c898c4dcce0282e1e86d9728140e5d0dc5d2a
Dominik Smoszna <dominiksmoszna@gmail.com>
2026-04-11 13:31:29 +0200

    feat: add DomainConfig

---
+++ src/main/java/com/github/dominiksmoszna/stocktracker/infrastructure/config/DomainConfig.java
@@ -0,0 +1,56 @@
+package com.github.dominiksmoszna.stocktracker.infrastructure.config;
+
+import com.github.dominiksmoszna.stocktracker.domain.port.out.*;
+import com.github.dominiksmoszna.stocktracker.domain.service.AlertService;
+import com.github.dominiksmoszna.stocktracker.domain.service.MarketDataService;
+import com.github.dominiksmoszna.stocktracker.domain.service.WatchlistService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainConfig {
+
+    @Bean
+    public WatchlistService watchlistService(
+            LoadWatchlistPort loadWatchlistPort,
+            ManageWatchlistPort manageWatchlistPort,
+            StockSubscriptionPort stockSubscriptionPort
+            ) {
+        return new WatchlistService(
+                loadWatchlistPort,
+                manageWatchlistPort,
+                stockSubscriptionPort
+        );
+    }
+
+    @Bean
+    public MarketDataService marketDataService(
+            CurrentPricePort currentPricePort,
+            SaveCurrentPricePort saveCurrentPricePort,
+            PriceHistoryPort priceHistoryPort,
+            PriceUpdatePort priceUpdatePort,
+            SendNotificationPort sendNotificationPort,
+            LoadAlertPort loadAlertPort
+    ) {
+        return new MarketDataService(
+                currentPricePort,
+                saveCurrentPricePort,
+                priceHistoryPort,
+                priceUpdatePort,
+                sendNotificationPort,
+                loadAlertPort
+        );
+    }
+
+    @Bean
+    public AlertService alertService(
+            LoadAlertPort loadAlertPort,
+            ManageAlertPort manageAlertPort
+    ) {
+        return new AlertService(
+                loadAlertPort,
+                manageAlertPort
+        );
+    }
+
+}
