adf670565227c49cdf993783293fc9d0818960af
Dominik Smoszna <dominiksmoszna@gmail.com>
2026-04-13 13:32:48 +0200

    feat: move websocket logic to HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,37 @@
+import {useState, useEffect} from 'react'
+import {Client} from '@stomp/stompjs'
+import SockJS from 'sockjs-client'
+
 function HomePage() {
-  return <h1>HomePage</h1>
+    const [prices, setPrices] = useState({})
+
+    useEffect(()=> {
+        const client = new Client({
+            webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+            onConnect: () => {
+                client.subscribe('/topic/prices', (message) => {
+                    const price = JSON.parse(message.body)
+                    setPrices(prev => ({
+                        ...prev,
+                        [price.ticker]:price
+                        }))
+                    })
+                }
+            })
+            client.activate()
+            return () => client.deactivate()
+        }, [])
+
+  return (
+      <div>
+          <h1>Stock Tracker</h1>
+          {Object.values(prices).map(price=>(
+              <div key={price.ticker}>
+                  {price.ticker}: {price.price}
+              </div>
+          ))}
+      </div>
+  )
 }
 
 export default HomePage
