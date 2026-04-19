cfbdb98de445ea7c08df7a88eea203a8228b2e48
Dominik Smoszna <dominiksmoszna@gmail.com>
2026-04-19 23:52:22 +0200

    feat: implement DatabaseAlertAdapter with JPA

--- src/main/java/com/github/dominiksmoszna/stocktracker/adapter/out/database/DatabaseAlertAdapter.java
+++ src/main/java/com/github/dominiksmoszna/stocktracker/adapter/out/database/DatabaseAlertAdapter.java
@@ -1,8 +1,12 @@
 package com.github.dominiksmoszna.stocktracker.adapter.out.database;
 
+import com.github.dominiksmoszna.stocktracker.adapter.out.database.entity.AlertEntity;
+import com.github.dominiksmoszna.stocktracker.adapter.out.database.repository.AlertJpaRepository;
+import com.github.dominiksmoszna.stocktracker.common.exception.PriceAlertNotFoundException;
 import com.github.dominiksmoszna.stocktracker.domain.model.PriceAlert;
 import com.github.dominiksmoszna.stocktracker.domain.port.out.LoadAlertPort;
 import com.github.dominiksmoszna.stocktracker.domain.port.out.ManageAlertPort;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -11,38 +15,70 @@ import java.util.UUID;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class DatabaseAlertAdapter implements LoadAlertPort, ManageAlertPort {
+
+    private final AlertJpaRepository repository;
+
     @Override
     public List<PriceAlert> loadPriceAlerts(UUID userId) {
         log.info("Loading price alerts for user {}", userId);
-        return List.of();
+        return repository.findAllByUserId(userId).stream().map(this::toDomain).toList();
     }
 
     @Override
     public List<PriceAlert> loadPriceAlertsByTicker(UUID userId, String ticker) {
         log.info("Loading price alerts for user {} by ticker {}", userId, ticker);
-        return List.of();
+        return repository.findAllByUserIdAndTicker(userId, ticker).stream().map(this::toDomain).toList();
     }
 
     @Override
     public PriceAlert loadPriceAlert(UUID alertId) {
         log.info("Loading price alert {}", alertId);
-        return null;
+        return repository.findById(alertId).map(this::toDomain).orElse(null);
     }
 
     @Override
     public List<PriceAlert> loadPriceAlertsByTicker(String ticker) {
         log.info("Loading price alerts for ticker {}", ticker);
-        return List.of();
+        return repository.findAllByTicker(ticker).stream().map(this::toDomain).toList();
     }
 
     @Override
     public void addAlert(PriceAlert priceAlert) {
+        AlertEntity entity = toEntity(priceAlert);
+        repository.save(entity);
         log.info("Adding alert {}", priceAlert);
     }
 
     @Override
     public void removeAlert(UUID alertId) {
+        if(!repository.existsById(alertId)) {
+            throw new PriceAlertNotFoundException("No alert with id " + alertId + " found");
+        }
+        repository.deleteById(alertId);
         log.info("Removing alert {}", alertId);
     }
+
+    private AlertEntity toEntity(PriceAlert alert) {
+        return new AlertEntity(
+                alert.getId(),
+                alert.getUserId(),
+                alert.getTicker(),
+                alert.getThreshold(),
+                alert.getType()
+        );
+    }
+
+    private PriceAlert toDomain(AlertEntity alert) {
+        return new PriceAlert(
+                alert.getId(),
+                alert.getUserId(),
+                alert.getTicker(),
+                alert.getThreshold(),
+                alert.getType()
+        );
+    }
+
+
 }
